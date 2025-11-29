b566ada1395208b78be11a4e7b9148d89d5d906b
Alex Pestov <alexpestov89@yandex.ru>
2025-11-29 22:10:06 +0300

    Добавлен shape

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -2,11 +2,18 @@ package hexlet.code.schemas;
 
 import java.util.Map;
 
-public class MapSchema extends BaseSchemaRequired<MapSchema> implements BaseSchema<Map>{
+public class MapSchema extends BaseSchemaRequired<MapSchema> implements BaseSchema<Map> {
     private int size;
+    private Map<Object, BaseSchema> schemas;
 
     public MapSchema sizeOf(int size) {
         this.size = size;
+
+        return this;
+    }
+
+    public MapSchema shape(Map schemas) {
+        this.schemas = schemas;
 
         return this;
     }
@@ -19,6 +26,22 @@ public class MapSchema extends BaseSchemaRequired<MapSchema> implements BaseSche
 
         if (map == null || size == 0) {
             return true;
+        }
+
+        if (schemas != null) {
+            for (var entry : schemas.entrySet()) {
+                var checkedKey = entry.getKey();
+                var schema = entry.getValue();
+                var checkedValue = map.get(checkedKey);
+
+                if (map.get(checkedKey) == null) {
+                    continue;
+                }
+
+                if (!schema.isValid(checkedValue)) {
+                    return false;
+                }
+            }
         }
 
         return map.size() == size;
--- app/src/test/java/hexlet/code/schemas/MapSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/MapSchemaTest.java
@@ -14,7 +14,7 @@ class MapSchemaTest {
         var schema = new MapSchema();
 
         assertTrue(schema.isValid(null));
-        assertTrue(schema.isValid(Map.of(1,2,3,4)));
+        assertTrue(schema.isValid(Map.of("1","2","3","4")));
     }
 
     @Test
@@ -25,7 +25,7 @@ class MapSchemaTest {
         schema.required();
 
         assertFalse(schema.isValid(null));
-        assertTrue(schema.isValid(Map.of(1,2,3,4)));
+        assertTrue(schema.isValid(Map.of("1","2","3","4")));
     }
 
     @Test
@@ -38,8 +38,8 @@ class MapSchemaTest {
 
         assertTrue(schema.isValid(null));
         assertFalse(schema.isValid(Map.of()));
-        assertTrue(schema.isValid(Map.of(1,2,3,4)));
-        assertFalse(schema.isValid(Map.of(1,2,3,4, 5, 6)));
+        assertTrue(schema.isValid(Map.of("1","2","3","4")));
+        assertFalse(schema.isValid(Map.of("1","2","3","4", "5", "6")));
     }
 
     @Test
@@ -50,10 +50,27 @@ class MapSchemaTest {
 
         schema.required().sizeOf(size);
 
-
         assertFalse(schema.isValid(null));
         assertFalse(schema.isValid(Map.of()));
-        assertTrue(schema.isValid(Map.of(1,2,3,4)));
-        assertFalse(schema.isValid(Map.of(1,2,3,4, 5, 6)));
+        assertTrue(schema.isValid(Map.of("1","2","3","4")));
+        assertFalse(schema.isValid(Map.of("1","2","3","4", "5", "6")));
+    }
+
+    @Test
+    @DisplayName("Проверка shape")
+    void shapeTest() {
+        var schema = new MapSchema();
+        var schemasMap = Map.of(
+                "name", new StringSchema().required().minLength(2).contains("ad").minLength(1),
+                "age", new NumberSchema().required().positive().range(10, 20)
+        );
+        var checkedMap = Map.of(
+                "name", "Vlad",
+                "age", 15
+        );
+
+        schema.shape(schemasMap);
+
+        assertTrue(schema.isValid(checkedMap));
     }
 }
